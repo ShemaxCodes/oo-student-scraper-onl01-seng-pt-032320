f3c858caca8127eac54730bc4cf9dad9f273f3ed
Jashema Panapa <jcpanapa@gmail.com>
2020-05-02 14:52:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = open(index_url) #grabs the html 
+    doc = Nokogiri::HTML(html) #saves the nested nodes into a variable doc 
+  
+    doc.css(".student-card").text.each do |student| 
+      student_info = {}
+      
+  
   end
 
   def self.scrape_profile_page(profile_url)
